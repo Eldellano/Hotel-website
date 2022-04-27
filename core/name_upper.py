5f90986296620f50a115a0ebd1002aec732bdf27
Vladislav Kalman <Vladislav_993@mail.ru>
2022-04-27 20:25:08 +0300

    refactoring name_upper func

--- core/name_upper.py
+++ core/name_upper.py
@@ -3,7 +3,6 @@ def name_upper(name):
     try:
         # Если имя из двух частей (имя/отчество)
         a, b = name.split()
-        get_full_name = lambda a, b: a[0].upper() + a[1:].lower() + ' ' + b[0].upper() + b[1:].lower()
-        return get_full_name(a, b)
+        return a[0].upper() + a[1:].lower() + ' ' + b[0].upper() + b[1:].lower()
     except ValueError:
         return name[0].upper() + name[1:].lower()
