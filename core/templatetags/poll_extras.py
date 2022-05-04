8b9fcdac75da84d6b893228bb679eae6d54a29bc
Vladislav Kalman <Vladislav_993@mail.ru>
2022-05-05 00:21:25 +0300

    add range filter

--- core/templatetags/poll_extras.py
+++ core/templatetags/poll_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from typing import Sequence
 
 
 register = template.Library()
@@ -7,6 +8,12 @@ register = template.Library()
 
 @register.filter(name='cut')
 @stringfilter
-def cut(value):
+def cut(value: str) -> str:
     """Для обрезки 'core' у src картинки"""
     return value[4:]
+
+
+@register.filter(name='range')
+def filter_range(number: int) -> Sequence:
+    """Для построения последовательности на основе длины объекта"""
+    return range(number)
